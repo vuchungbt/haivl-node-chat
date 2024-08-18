ecc3289b1adf4496c69d9fbd867e15562cfd0086
thobd <buiductho195@gmail.com>
2024-08-18 16:41:23 +0700

    Setup database
    Signed-off-by: thobd <buiductho195@gmail.com>

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('DATABASE_URI'),
+        dbName: configService.get<string>('DATABASE_NAME'),
+        user: configService.get<string>('DATABASE_USER'),
+        pass: configService.get<string>('DATABASE_PASSWORD'),
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
